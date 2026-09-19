d85c5eab8f8d910426ee1631854c68ac15500da9
lhommedumilieu <service@3-wm.net>
2026-09-19 06:07:45 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import TawkChat from './components/TawkChat.jsx'
 import Home from './pages/Home.jsx'
 import Services from './pages/Services.jsx'
 import Boutique from './pages/Boutique.jsx'
 import BlogIndex from './pages/BlogIndex.jsx'
 import BlogPost from './pages/BlogPost.jsx'
+import Connexion from './pages/Connexion.jsx'
+import Inscription from './pages/Inscription.jsx'
+import EspaceMembre from './pages/EspaceMembre.jsx'
 import APropos from './pages/APropos.jsx'
 import Contact from './pages/Contact.jsx'
 import MentionsLegales from './pages/MentionsLegales.jsx'
@@ -24,6 +28,7 @@ export default function App() {
   return (
     <>
       <ScrollToTop />
+      <TawkChat />
       <Header />
       <main>
         <Routes>
@@ -32,6 +37,9 @@ export default function App() {
           <Route path="/boutique" element={<Boutique />} />
           <Route path="/blog" element={<BlogIndex />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/connexion" element={<Connexion />} />
+          <Route path="/inscription" element={<Inscription />} />
+          <Route path="/espace-membre" element={<EspaceMembre />} />
           <Route path="/a-propos" element={<APropos />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/mentions-legales" element={<MentionsLegales />} />
